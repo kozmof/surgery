7a7b9c633b416c53926ba884992c49636a9e379c
Kozo Oeda <kozo.oeda@gmail.com>
2018-11-10 01:38:04 +0900

    Fix

--- surgery.py
+++ surgery.py
@@ -5,7 +5,6 @@ from types import FunctionType
 
 
 def surgery(f):
-    @functools.wraps(f)
     def get_root_arg_info(f):
         return {argname: num for num, argname in enumerate(f.__code__.co_varnames[:f.__code__.co_argcount])}
 
@@ -13,9 +12,9 @@ def surgery(f):
         arg_dict = get_root_arg_info(f)
 
         arg_pos = []
-        for freevars in co_const.co_freevars:
-            if freevars in arg_dict:
-                arg_pos.append(arg_dict[freevars])
+        for freevar in co_const.co_freevars:
+            if freevar in arg_dict:
+                arg_pos.append(arg_dict[freevar])
 
         PyCellNew = ctypes.pythonapi.PyCell_New
         ctypes.pythonapi.PyCell_New.argtypes = [ctypes.py_object]
@@ -33,6 +32,7 @@ def surgery(f):
 
         return f_list
 
+    @functools.wraps(f)
     def core(*args):
         inner_f_names = [co_const.co_name for co_const in f.__code__.co_consts if iscode(co_const)]
         inner_f_cores = make_inner_f_cores(f, *args)
