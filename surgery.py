33e32370051d233ed1a095e002bed76a9582376e
Kozo Oeda <kozo.oeda@gmail.com>
2019-03-18 21:13:47 +0900

    Add a switching feature

--- surgery.py
+++ surgery.py
@@ -4,40 +4,50 @@ from inspect import iscode
 from types import FunctionType
 
 
-def surgery(f):
-    def get_root_arg_info(f):
-        return {argname: num for num, argname in enumerate(f.__code__.co_varnames[:f.__code__.co_argcount])}
+def surgery(test=True):
+    def surgery_core(f):
+        def get_root_arg_info(f):
+            return {argname: num for num, argname in enumerate(f.__code__.co_varnames[:f.__code__.co_argcount])}
 
-    def make_closure(f, co_const, *args):
-        arg_dict = get_root_arg_info(f)
+        def make_closure(f, co_const, *args):
+            arg_dict = get_root_arg_info(f)
 
-        arg_pos = []
-        for freevar in co_const.co_freevars:
-            if freevar in arg_dict:
-                arg_pos.append(arg_dict[freevar])
+            arg_pos = []
+            for freevar in co_const.co_freevars:
+                if freevar in arg_dict:
+                    arg_pos.append(arg_dict[freevar])
 
-        PyCellNew = ctypes.pythonapi.PyCell_New
-        PyCellNew.argtypes = [ctypes.py_object]
-        PyCellNew.restype = ctypes.py_object
-        cells = tuple(PyCellNew(arg) for pos, arg in enumerate(args) if pos in arg_pos)
-        return cells
+            PyCellNew = ctypes.pythonapi.PyCell_New
+            PyCellNew.argtypes = [ctypes.py_object]
+            PyCellNew.restype = ctypes.py_object
+            cells = tuple(PyCellNew(arg) for pos, arg in enumerate(args) if pos in arg_pos)
+            return cells
 
-    def make_inner_f_cores(f, *args):
-        f_list = []
-        for co_const in f.__code__.co_consts:
-            if iscode(co_const) and co_const.co_freevars:
-                f_list.append(FunctionType(co_const, f.__globals__, name=co_const.co_name, closure=make_closure(f, co_const, *args)))
-            elif iscode(co_const) and not co_const.co_freevars:
-                f_list.append(FunctionType(co_const, f.__globals__, name=co_const.co_name))
+        def make_inner_f_cores(f, *args):
+            f_list = []
+            for co_const in f.__code__.co_consts:
+                if iscode(co_const) and co_const.co_freevars:
+                    f_list.append(FunctionType(co_const, f.__globals__, name=co_const.co_name, closure=make_closure(f, co_const, *args)))
+                elif iscode(co_const) and not co_const.co_freevars:
+                    f_list.append(FunctionType(co_const, f.__globals__, name=co_const.co_name))
 
-        return f_list
+            return f_list
 
-    @functools.wraps(f)
-    def core(*args):
-        inner_f_names = [co_const.co_name for co_const in f.__code__.co_consts if iscode(co_const)]
-        inner_f_cores = make_inner_f_cores(f, *args)
-        inner_f_dict = dict(zip(inner_f_names, inner_f_cores))
-        return inner_f_dict
+        @functools.wraps(f)
+        def core(*args):
+            inner_f_names = [co_const.co_name for co_const in f.__code__.co_consts if iscode(co_const)]
+            inner_f_cores = make_inner_f_cores(f, *args)
+            inner_f_dict = dict(zip(inner_f_names, inner_f_cores))
+            return inner_f_dict
 
-    return core 
+        @functools.wraps(f)
+        def pass_f(*args, **kwargs):
+            return f(*args, **kwargs)
+
+        if test:
+            return core 
+        else:
+            return pass_f
+
+    return surgery_core
 
--- test/test.py
+++ test/test.py
@@ -4,7 +4,11 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 from surgery import surgery
 
 
-@surgery
+global surgery_on
+surgery_on = True
+
+
+@surgery(surgery_on)
 def test1(x, y, z):
     def test2(a):
         return 10 + a + x
@@ -19,17 +23,21 @@ def test1(x, y, z):
     def test5(a, b, c, d):
         return a + b + c + d
 
-    return 100
+    return x + y + z
 
 
 class Test(unittest.TestCase):
     def test_return(self):
-        inner_f = test1(50, 100, 200)
-        self.assertEqual(inner_f['test2'](100), 160)
-        self.assertEqual(inner_f['test3'](), 100)
-        self.assertEqual(inner_f['test4'](), 1200)
-        self.assertEqual(inner_f['test5'](1, 2, 3, 4), 10)
-        
-
+        if surgery_on:
+            print("surgery is active")
+            inner_f = test1(50, 100, 200)
+            self.assertEqual(inner_f['test2'](100), 160)
+            self.assertEqual(inner_f['test3'](), 100)
+            self.assertEqual(inner_f['test4'](), 1200)
+            self.assertEqual(inner_f['test5'](1, 2, 3, 4), 10)
+        else:
+            print("surgery is inactive")
+            self.assertEqual(test1(50, 100, 200), 350)
+    
 if __name__ == "__main__":
     unittest.main()
